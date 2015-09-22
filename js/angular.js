7dcc0adca8aea396683e073e668629aa8ef15836
brandon932 <bthornton_932@yahoo.com>
2015-09-22 13:33:47 -0600

    added bootstrap and photos to buttons

--- js/angular.js
+++ js/angular.js
@@ -3,50 +3,52 @@
 var app = angular.module('myApp', []);
 
 app.controller('myController', function($scope) {
-   
-   $scope.win = 0;
-   $scope.lose = 0;
-   $scope.message = "";
-   
 
-   $scope.computer = function() {
-   	var symbols = ["rock", "paper", "scissors"];
-   	var randomNumber = Math.floor(Math.random() * symbols.length);
-   	$scope.computerChoice = symbols[randomNumber];
-   };
+  $scope.win = 0;
+  $scope.lose = 0;
+  $scope.tie = 0;
+  $scope.message = "";
 
 
-   $scope.howToWin = function(choice) {
-   	var usersChoice = choice;
-   	$scope.choice = choice;
-   	$scope.computer();
-   	$scope.computer();
-   	if (usersChoice === $scope.computerChoice) {
-   		$scope.message = "You tied!";
-   		return;
-   	} else if (usersChoice === "rock" && $scope.computerChoice === "paper") {
-   		$scope.win += 1;
-   		$scope.message = "You won!!";
-   	} else if (usersChoice === "scissors" && $scope.computerChoice === "paper") {
-   		$scope.win += 1;
-   		$scope.message = "You won!!";
-   	} else if (usersChoice === "paper" && $scope.computerChoice === "rock") {
-   		$scope.win += 1;
-   		$scope.message = "You won!!";
-   	} else {
-   		$scope.lose += 1;
-   		$scope.message = "Computer won!!";
-   	} 
-   };
+  $scope.computer = function() {
+    var symbols = ["rock", "paper", "scissors"];
+    var randomNumber = Math.floor(Math.random() * symbols.length);
+    $scope.computerChoice = symbols[randomNumber];
+  };
 
 
-   $scope.reset = function() {
-   	console.log("test");
-   	$scope.win = 0,
-   	$scope.lose = 0
-   	$scope.choice = "",
-   	$scope.computerChoice = "",
-   	$scope.message = ""
-   };
+  $scope.howToWin = function(choice) {
+    var usersChoice = choice;
+    $scope.choice = choice;
+    $scope.computer();
+    $scope.computer();
+    if (usersChoice === $scope.computerChoice) {
+      $scope.tie +=1;
+      $scope.message = "You tied!";
+      return;
+    } else if (usersChoice === "rock" && $scope.computerChoice === "paper") {
+      $scope.win += 1;
+      $scope.message = "You won!!";
+    } else if (usersChoice === "scissors" && $scope.computerChoice === "paper") {
+      $scope.win += 1;
+      $scope.message = "You won!!";
+    } else if (usersChoice === "paper" && $scope.computerChoice === "rock") {
+      $scope.win += 1;
+      $scope.message = "You won!!";
+    } else {
+      $scope.lose += 1;
+      $scope.message = "Computer won!!";
+    }
+  };
+
+
+  $scope.reset = function() {
+    $scope.win = 0,
+    $scope.lose = 0,
+    $scope.choice = "",
+    $scope.computerChoice = "",
+    $scope.message = "",
+    $scope.tie = 0
+  };
 
 });
